3b1400c6ad3adc83c09ce72c6496c283d68c7a12
karunmv <karunmanoj123@gmail.com>
2025-11-28 15:12:41 -0500

    Added timer; tried debouncing not great

--- ssd1306_128x32_i2c/ssd1306_128x32_i2c.ino
+++ ssd1306_128x32_i2c/ssd1306_128x32_i2c.ino
@@ -1,26 +1,20 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-<<<<<<< HEAD
-// #include <TimeLib.h>
-
-#define OLED_ADDR 0x3C
-Adafruit_SSD1306 display(128, 32, &Wire, -1);
-
-void setup() {
-  Wire.begin(1,0);
-=======
 #include "Adafruit_MPR121.h"
 // #include <TimeLib.h>
 
 #define OLED_ADDR   0x3C
-Adafruit_SSD1306 display(128, 32, &Wire, -1);
+#define DEBOUNCE_DELAY 50
+#define TOUCH_SENSOR_THRESHOLD 6
 
+Adafruit_SSD1306 display(128, 32, &Wire, -1);
 Adafruit_MPR121 cap = Adafruit_MPR121();
 
-int counter = 0;
 int started = 0;
 int stopped = 0;
+int touched = 0;
+unsigned long static startTime = 0;
 
 void setup() {
   
@@ -43,40 +37,72 @@ void setup() {
   display.clearDisplay();
   display.setTextColor(INVERSE);
   display.setTextSize(2.5);
-  display.setCursor(15,15);
-  display.println("0:00.000");
+  display.setCursor(5,15);
+  display.println("00:00.000");
   display.display();
+
+
 }
 
 void loop() {
 
-  if ((cap.filteredData(0) < 6) && (cap.filteredData(1) < 6) && !started) {
+  // unsigned long timeNow = millis();
 
-    while ((cap.filteredData(0) < 6) && (cap.filteredData(1) < 6));
+  // // Debouncing
+  // if((millis() - timeNow) > DEBOUNCE_DELAY){
+  //   touched = 1;
+  // }
+  
+  // if ((cap.filteredData(0) < TOUCH_SENSOR_THRESHOLD) && (cap.filteredData(1) < TOUCH_SENSOR_THRESHOLD)) {
+  //   unsigned long touch_time = millis();
+  //   if((millis() - touch_time) > DEBOUNCE_DELAY) touched = 1;
+  // }
+  
+  // Start timer only if both sensors are touched simultaneously for a period longer than debounce delay
+  if ((cap.filteredData(0) < TOUCH_SENSOR_THRESHOLD) && (cap.filteredData(1) < TOUCH_SENSOR_THRESHOLD) && !started) {
+    unsigned long static touch_time = millis();
+    if((millis() - touch_time) > DEBOUNCE_DELAY){
+      while ((cap.filteredData(0) < TOUCH_SENSOR_THRESHOLD) && (cap.filteredData(1) < TOUCH_SENSOR_THRESHOLD));
+      started = 1;
+      touched = 0;
+      startTime = millis();
+    }
 
-    started = 1;
-    counter = 0;
   }
 
   if (started) {
-    if ((cap.filteredData(0) < 6) && (cap.filteredData(1) < 6)) {
+    if ((cap.filteredData(0) < TOUCH_SENSOR_THRESHOLD) && (cap.filteredData(1) < TOUCH_SENSOR_THRESHOLD)) {
 
-      while ((cap.filteredData(0) < 6) && (cap.filteredData(1) < 6));
-
+      while ((cap.filteredData(0) < TOUCH_SENSOR_THRESHOLD) && (cap.filteredData(1) < TOUCH_SENSOR_THRESHOLD));
       started = 0;
     }
+    
+    // Find elapsed time from start of stopwatch
+    unsigned long elapsed = millis() - startTime;
+
+    unsigned int minutes = elapsed / 60000;
+    unsigned int seconds = (elapsed % 60000) / 1000;
+    unsigned int milliseconds = elapsed % 1000;
 
     display.clearDisplay();
     display.setTextColor(INVERSE);
     display.setTextSize(2.5);
-    display.setCursor(15,15);
-    display.print(counter / 100);
+    display.setCursor(5,15);
+
+
+    /* Display the time on LCD screen */
+    if(minutes<10) display.print("0"); // Padding zeros
+    display.print(minutes);
+    display.print(":");
+    if(seconds<10) display.print("0"); // Padding zeros
+    display.print(seconds);
     display.print(".");
-    display.print(counter % 100);
+    if(milliseconds<10) display.print("00"); // Padding zeros
+    if(milliseconds<100) display.print("0"); // Padding zeros
+    display.print(milliseconds);
+
     display.display();
 
-    counter++;
-    
     // put a delay so it isn't overwhelming
     delay(10);
   }
